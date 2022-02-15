eba98398aa4acb2c986a21959d2d6a4ccebb2f73
Thiago Artur Schumann <thiagoarturschumann@gmail.com>
2022-02-15 19:48:41 -0300

    registro dos models no admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Expanse, Bank, BankAccount
+from .models import Expanse, Bank, BankAccount, Transaction
+
 
 # Register your models here.
-admin.site.register(Expanse)
-admin.site.register(Bank)
-admin.site.register(BankAccount)
+
+@admin.register(Expanse)
+class ExpanseAdmin(admin.ModelAdmin):
+    list_display = ("next_occurrence", "periodicity_occurrence", "value")
+
+
+@admin.register(Bank)
+class BankAdmin(admin.ModelAdmin):
+    list_display = ("name", "logo")
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ("name", "opening_balance")
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("due_date", "settlement_date", "status", "value")
